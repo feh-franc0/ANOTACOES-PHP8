ef8f3c2b7ad7947211d5c38211123161281d98a3
feh-franc0 <fernandofv1110@gmail.com>
2021-12-10 15:26:19 -0300

    Contantes define(nome, valor)

--- 1FundamentosPHP8/4-Constantes/index.php
+++ 1FundamentosPHP8/4-Constantes/index.php
@@ -7,4 +7,11 @@
 /**os parametros para uma constante sao o nome e o valor da constante */
 define("CURSO", "PHP 8 da Hcode");
 
+//esse CURSO se refere ao "PHP 8 da Hcode", sendo esse uma constante
 echo CURSO;
+
+echo "<br/>";
+
+define("ESTUDE", "Nunca deixe de estudar, constancia e foco sempre irão ser a melhor forma de chegar ao seu objetivo!");
+
+echo ESTUDE;
