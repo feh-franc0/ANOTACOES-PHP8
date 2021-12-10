70670a0b15f5f3295bce6097f828a2808c81281c
feh-franc0 <fernandofv1110@gmail.com>
2021-12-10 13:52:51 -0300

    FUNDAMENTOS : 1-Tags PHP e HTML

---
+++ 1FundamentosPHP8/1-Tags PHP e HTML/index.php
@@ -0,0 +1,20 @@
+<h1>Texto HTML</h1>
+
+<?php
+/*quando voce tiver um arquivo PHP use apenas a tag de abertura(<?php)
+*/
+/*quando for usar em um arquivo HTML use a tag de abertura(<?php) e a de fechamento (?>)
+*/
+/*TAG PHP ABREVIADA
+usadas para abrir e fechar o php em outros tipos de arquivos de maneira abreviada
+Tag de abertura : <?=
+Tag dde fechamento : ?>
+-É possível habilitar a tag curta do PHP sem o nome php na abertura da tag, por meio da diretiva short_open_tag.
+Entretando essa opção é desencorajada pela documentação oficial e boas práticas.
+*/
+echo "<h2>Tags php e html</h2>";
+?>
+
+<p>Estudando <?="PHP8" ?></p>
+
+<!--SE TIVER APENAS PHP NESSE AQUIVO SO DEIXE A TAG DE ABERTURA DO PHP "< ?php"-->
