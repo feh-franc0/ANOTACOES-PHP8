46597a50cc026fed013c6e5351f0e7d2be9d8347
feh-franc0 <fernandofv1110@gmail.com>
2021-12-10 14:57:57 -0300

    Comentários em PHP, usando a extensão Better Commnets

---
+++ 1FundamentosPHP8/2-Comentários/index.php
@@ -0,0 +1,19 @@
+<?php
+/**COMENTÁRIOS:
+ * São textos que não seão interpretados como comandos da liguagem PHP
+ * e podem ser úteis para explicar ou desabilitar sem excluir trechos do código
+ */
+
+//Há duas maneiras de criar comentários no PHP:
+
+//Comentários de Linha, use //
+/** Comentários de Bloco, use /** */
+
+/** EXTENSÃO : Better Comments 
+ * Normal
+ * *Comentário destacado
+ * ! Comentário de alerta
+ * ? Comentário de Duvida
+ * TODO : Comentário de a fazer
+ * @param nome Informar o nome dos parametros
+ */
