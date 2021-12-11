c75d64a05556450440eb6b619893017858967c81
feh-franc0 <fernandofv1110@gmail.com>
2021-12-11 12:57:10 -0300

    Operadores de Atribuição, comentando melhor sobre

--- 2Operadores/1-Operadores de Atribuição/index.php
+++ 2Operadores/1-Operadores de Atribuição/index.php
@@ -1,13 +1,21 @@
 <?php
-
+//operador de atribuição "="
 $amount = 200.00;
 $valueA = $amount;
 
-//$amount /= 2; //100
-$discount = 33;
+//$amount /= 2; // $amount=$amount/2 -> $amount=100
+$discount = 10;
+//tmb podemos usar os operadores atribuição com alguns operadores aritmeticos,como o +,-,*,/ e etc..Que alem de atribuir ira fazer a operação
 $formula = $discount / 100;
 
-$valueA *= $formula;
+$valueA *= $formula; //10% do $valueA que é = 20
+echo $valueA;
+echo "</br>";
 $amount -= $valueA;
+// = , += , -= , *= , /= , .=
+echo $amount; // = 180
 
-echo $amount;
+/*Operadores de Atribuição por referência, já vimos isso em aulas passadas.
+$a = 3;
+$b = &$a; // $b é uma referência de $a, se mudar o valor de $a o $b muda automaticamente
+*/
