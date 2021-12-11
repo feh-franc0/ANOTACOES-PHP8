69b6f1ee6c4be9606632e0f28fd824d035780ba5
feh-franc0 <fernandofv1110@gmail.com>
2021-12-11 20:27:22 -0300

    Exercício Prático

---
+++ 2Operadores/Exercício Prático - Operadores/index.php
@@ -0,0 +1,8 @@
+<?php
+/**EXERCÍCIOS PRÁTICOS:
+ * 
+ * TODO: Crie três variáveis: preço, desconto e frete sendo os valores 1000.00, 10 e 35.49 respectivamente.
+ * TODO: Em uma linha crie uma variável chamada total que conterá o desconto aplicado ao valor e no final somado o frete.
+ * TODO: Crie uma variável chamada compra que conterá um valor boolean verdadeiro se o valor da variável total for até o valor do preço e menor que mil e vinte
+ * TODO: Exibir com var_dump no final as variáveis total e compra.
+ */
