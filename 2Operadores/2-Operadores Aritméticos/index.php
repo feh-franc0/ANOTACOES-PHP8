790d220ab47a22ce64de6de2f3398cbe7452e533
feh-franc0 <fernandofv1110@gmail.com>
2021-12-11 14:22:08 -0300

    Operadores Aritmeticos

--- 2Operadores/2-Operadores Aritméticos/index.php
+++ 2Operadores/2-Operadores Aritméticos/index.php
@@ -1,13 +1,13 @@
 <?php
 
 $value = "-10.5";
-$value = +$value;//changes to some type of number(identity)
+$value = +$value;//muda para algum tipo de número int ou float conforme o apropriado(identidade)/(identity)
 
-var_dump(-$value); //denial of value
+var_dump(-$value); //negação de valor, muda o positivo para negativo e o negativo para positivo
 
 var_dump(10 + 2); //=12
 var_dump(10 - 2); //=8
 var_dump(10 * 2); //=20
 var_dump(10 / 2); //=5
-var_dump(10 % 2); //=0 (very used to know if the number is even or odd)
+var_dump(10 % 2); //=0 (muito usado para saber se o número é par ou ímpar)
 var_dump(10 ** 2);//=100
