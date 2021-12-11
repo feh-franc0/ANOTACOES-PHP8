fa31786c34dd7d00811ba71a5cc1a5f993fb8cd8
feh-franc0 <fernandofv1110@gmail.com>
2021-12-11 14:38:46 -0300

    Precedencia de Operadores

--- 2Operadores/3-Precedência de Operadores/index.php
+++ 2Operadores/3-Precedência de Operadores/index.php
@@ -1,3 +1,24 @@
 <?php
+//Quem vem primeiro em cada operação 
+echo (3 + 5) * 3; // = 24
+echo 3 + 5 * 3; // = 18
 
-echo (3 + 5) * 3;
+/**Precedência
+ * PRIMEIROS A SEREM EXECUTADOS ...
+ * () a maior precedência de operadores
+ * clone new -> clone e new
+ * [] -> array()
+ * * * -> aritmética
+ * ++ -- ~ (int)(float)(string)(array)(object)(bool) @ -> types e incrementodecremento
+ * instanceof -> tipos
+ * ! -> lógicos
+ * * / % -> aritmética
+ * || -> lógicos
+ * ?? -> comparação
+ * ? : -> ternário
+ * = += -= *= **= /= .= %= &= |= ^= <<= >>= -> Atribuição
+ * and -> Lógicos
+ * xor -> Lõgicos
+ * or -> Lõgicos
+ * ULTIMOS A SEREM EXECUTADOS...
+ */
