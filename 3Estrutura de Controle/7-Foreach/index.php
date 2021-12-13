9b25c15b15d35e049281979f47f0f2d45245ae89
feh-franc0 <fernandofv1110@gmail.com>
2021-12-13 14:35:30 -0300

    Foreach, pegando o index e o valor e apenas o valor

---
+++ 3Estrutura de Controle/7-Foreach/index.php
@@ -0,0 +1,20 @@
+<?php
+
+/**FOREACH
+ * O comando foreach fornece uma maneira mais fácil de iterar em arrays, repetindo códigos para cada índice de um array.
+ * 
+ * É possível obter o valor de cada item do Array ou o nome da chave e o valor de cada item do Array
+ * 
+ */
+
+ $aereas = ["a" => "Gol",
+            "b" => "Latam",
+            "Azul"];
+
+foreach ($aereas as $chave => $valor) { //usando $index => $valor . ira mostrar o indice do arrey e o valor que o contem
+    var_dump($chave, $valor);
+}
+
+foreach ($aereas as $valor) {
+    var_dump($valor); //mostrara apenas o valor
+}
