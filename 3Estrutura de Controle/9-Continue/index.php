b260241792116870d3077c11c999255f19bbe648
feh-franc0 <fernandofv1110@gmail.com>
2021-12-13 15:38:36 -0300

    Continue

---
+++ 3Estrutura de Controle/9-Continue/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**CONTINUE
+ * O comando continue é usado para pular o resto de código do resttante da iteração atual e continuar na próxima iterção.
+ * 
+ * Este comando também aceita um argumento numérico opcional que diz quantos níveis de aninhados deverão continuar.
+ * 
+ */
+
+ for (
+    $contador = 0;
+    $contador <= 250;
+    $contador++
+ ) {
+     if($contador >= 118 && $contador <= 125){
+         continue; // vai ignorar a contagem de 118 até 125, não irá nem aparecer os números de 118 à 125. Tmb pode ser usado números dps igualmento o braak,caso esteja em aninhamento ccom outros continues
+     }
+     echo $contador." ";
+ }
