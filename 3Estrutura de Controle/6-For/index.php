3cd57f946222da3d1fc3304db3afcfd28ca98c08
feh-franc0 <fernandofv1110@gmail.com>
2021-12-13 13:30:27 -0300

    for e como podemos usar

---
+++ 3Estrutura de Controle/6-For/index.php
@@ -0,0 +1,13 @@
+<?php
+/**FOR
+ * O comando for repete um trecho de código com base em 3 expressões que definimos em seus parênteses.
+ * 1.Define o início da repetição
+ * 2.Define a condição que será avaliada para as repetições.
+ * 3.Define o que será feito no final de cada repetição.
+ * 
+ * ultiliamos mt o for para percorrer arrays e coleções.
+ */
+
+ for ($contador=0; $contador <= 10 ; $contador++) { 
+    var_dump($contador);
+ }
