eef9d693bc600315330caf9762078fd30e2aa9da
feh-franc0 <fernandofv1110@gmail.com>
2021-12-14 11:21:37 -0300

    Exercício Prático - Estrutura de Controle

---
+++ 3Estrutura de Controle/15-Exercício Prático - Estrutura de Controle/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * *Estrutura de Controles
+ * * IF, Switch, Match
+ * TODO: Crie três variáveis, $autorizado, $admin e $nome . Avalie se a variável $autorizado for igual a verdadeiro e $admin igual a verdadeiro escreva na tela: "Área Administrativa, $nome, Bem vindo!"
+ * 
+ * TODO: Crie quatro variáveis, $quantidade, $valorUnitario, $subtotal e $frete . A variável $subtotal receberá $quantidade multiplicado por $valorUnitario. Se o cliente comprar acima de 3 unidades aplique um desconto em $subtotal de 10% do valor da compra,caso contrário aplique um desconto de 5% . Em seguida será somado o valor de frete em subtotal Para a massa de teste utilize: $quantidade = 2; $valorUnitario = 1200.00; $frete = 40.00
+ * 
+ * *Loops e Contadores
+ * TODO: Crie um Contador para ir de 1920 a 2022.
+ * TODO: Este contador presica armazenar uma instruçao HTML dentro de uma variável string $option com a seguinte estrutura: '<option value="$valor">$valor</option>'.
+ * TODO: Crie uma condição dentro deste contador, se o $ano for igual a 2021 imprima o atributo selected conforme a estrutura a seguir: <option value="$valor" selected="selected">$valor</option>
+ * 
+ * *require e include
+ * TODO: Crie um arquivo chamado exercicio-03-a.php declarando duas constantes IDIOMA = "pt_BR"; LOCALE = "America/Sao_Paulo";
+ * TODO: Crie um arquivo chamado exercicio-03-b.php incluindo de forma obrigatória o arquivo anterior, e ccrie uma condição, para caso o IDIOMA seja "pt-BR" e o LOCALE seja "America/Sao_Paulo" Imprima na tela: Idioma: Português, Fuso Horário: -3:00
+ * 
+ */
