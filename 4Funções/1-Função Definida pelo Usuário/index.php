29b8ce9644a5632dd3d50c7a4a8c23b138fc5b97
feh-franc0 <fernandofv1110@gmail.com>
2021-12-14 19:08:24 -0300

    Função Definida pelo Usuário

---
+++ 4Funções/1-Função Definida pelo Usuário/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/**Função Definida pelo Usuário:
+ * Qualquer código PHP válido pode aparecer ddentro de uma função, mesmo outras funções e definições de classes.
+ * 
+ * Nomes de funções seguem as mesmas regras que outros rótulo no PHP,
+ * Um nome de funções válido começa com uma letra ou um sublinhado,seguido, seguido por qualquer número de letras, número ou sublinhado
+ * 
+ */
+
+ //Essa função que não tem o return é chamado de sub-rotina
+
+ function somar()
+ {
+     echo "2 + 2 = 4";
+ }
+
+ somar();
